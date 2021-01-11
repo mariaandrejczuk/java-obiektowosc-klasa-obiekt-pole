cf996d9abe5ae4db18f57bb5ab1171a2f0fba7b3
Maria Andrejczuk <Maria.Andrejczuk@gmail.com>
2021-01-11 14:10:16 +0100

    #1 rozwiązanie zadania Invoice

---
+++ src/Invoice.java
@@ -0,0 +1,11 @@
+public class Invoice {
+    int number;
+    char[] description;
+    int quantity;
+    double price;
+    double vat;
+    int fromNIP;
+    int forNIP;  //przygotowano klasę i pola
+
+
+}
